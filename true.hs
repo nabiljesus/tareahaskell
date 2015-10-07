8a384e4b2342a5678c2f7b745b99f7025a4f2da3
Nabil J. Marquez <nabiljesus@gmail.com>
2015-10-07 09:52:35 -0430

    Listo funcion vars. Revisar si es optimizable

--- true.hs
+++ true.hs
@@ -25,7 +25,7 @@ remove e k = if null tl then hd
                         else hd ++ tail tl
     where (hd,tl)  = span (\ (elem,_) -> k/=elem) e
 
--- Aux functions ---------------------------------------
+-- evalP Aux functions ---------------------------------------
 maybeBin :: (a->b->c) -> Maybe a -> Maybe b -> Maybe c
 maybeBin f (Just op1) (Just op2) = Just $ f op1 op2
 maybeBin f _ _                   = Nothing
@@ -33,7 +33,7 @@ maybeBin f _ _                   = Nothing
 maybeUn :: (a->b) -> Maybe a -> Maybe b
 maybeUn f (Just op1) = Just $ f op1
 maybeUn _ Nothing    = Nothing
---------------------------------------------------------
+---------------------------------------------------------------
 evalP :: Environment -> Proposition -> Maybe Bool
 evalP _ (Cons b)          = Just b
 evalP e (Var str)         = find e str
@@ -42,10 +42,23 @@ evalP e (And prop1 prop2) = maybeBin  (&&) (evalP e prop1)       (evalP e prop2)
 evalP e (Or prop1 prop2)  = maybeBin  (||) (evalP e prop1)       (evalP e prop2)
 evalP e (Imp prop1 prop2) = maybeBin  (||) (evalP e (Neg prop1)) (evalP e prop2)
 
+-- vars Aux functions ---------------------------------------
+union :: (Eq a) => [a] -> [a] -> [a]
+union [] b = b
+union a [] = a
+union a (b:bn) = if elem b a then union a bn
+                 else union (b:a) bn
+-------------------------------------------------------------
 vars :: Proposition -> [String]
-vars p = undefined -- Nab
+vars (Cons _)          = []
+vars (Var str)         = [str]
+vars (Neg prop)        = vars prop
+vars (And prop1 prop2) = union (vars prop1) $vars prop2
+vars (Or prop1 prop2)  = union (vars prop1) $vars prop2
+vars (Imp prop1 prop2) = union (vars prop1) $vars prop2
 
 isTautology :: Proposition -> Bool
 isTautology p = undefined -- Nab Jav
 
 b = (replicate 10000 ("b",True)) ++ [("a",False)] ++ (replicate 10000 ("b",True))
+unAnd = And (Or (Var "a") (Var "b")) (Imp (Cons True) (Neg (And (Var "b") (Var "i"))))
