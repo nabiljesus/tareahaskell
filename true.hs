548af28ee13842eaa46a0cf4df773fe39634d2a4
Javier Lopez <javierlopm@hotmail.com>
2015-10-07 16:26:20 -0430

    true.hs listo, tal vez mejorable

--- true.hs
+++ true.hs
@@ -1,3 +1,5 @@
+import Data.Maybe(fromJust)
+
 data Proposition = Cons Bool
                  | Var  String
                  | Neg  Proposition
@@ -39,8 +41,11 @@ evalP _ (Cons b)          = Just b
 evalP e (Var str)         = find e str
 evalP e (Neg prop)        = maybeUn   not  (evalP e prop)
 evalP e (And prop1 prop2) = maybeBin  (&&) (evalP e prop1)       (evalP e prop2)
-evalP e (Or prop1 prop2)  = maybeBin  (||) (evalP e prop1)       (evalP e prop2)
+evalP e (Or  prop1 prop2) = maybeBin  (||) (evalP e prop1)       (evalP e prop2)
 evalP e (Imp prop1 prop2) = maybeBin  (||) (evalP e (Neg prop1)) (evalP e prop2)
+
+
+
 
 -- vars Aux functions ---------------------------------------
 union :: (Eq a) => [a] -> [a] -> [a]
@@ -54,11 +59,57 @@ vars (Cons _)          = []
 vars (Var str)         = [str]
 vars (Neg prop)        = vars prop
 vars (And prop1 prop2) = union (vars prop1) $vars prop2
-vars (Or prop1 prop2)  = union (vars prop1) $vars prop2
+vars (Or  prop1 prop2) = union (vars prop1) $vars prop2
 vars (Imp prop1 prop2) = union (vars prop1) $vars prop2
 
-isTautology :: Proposition -> Bool
-isTautology p = undefined -- Nab Jav
 
-b = (replicate 10000 ("b",True)) ++ [("a",False)] ++ (replicate 10000 ("b",True))
-unAnd = And (Or (Var "a") (Var "b")) (Imp (Cons True) (Neg (And (Var "b") (Var "i"))))
+
+
+-- isTautology aux functions ----------------------------------------
+type Binary = [Bool]
+
+numToBinary :: Int -> Binary
+numToBinary 0 = [False]
+numToBinary n = convert n []
+                where convert num l = if num==0
+                                        then l
+                                        else convert q ((r==1):l)
+                        where (q,r) = quotRem num 2
+
+generateCase :: [String] -> Int -> Environment  -- Ambiente asociado
+generateCase vars n = snd $ foldr zipRight (numToBinary n,[]) vars
+    where zipRight var ([],acc)  = ([],(var,False):acc)
+          zipRight var (bin,acc) = (tail bin,(var,head bin):acc)
+
+isLastCase :: Environment -> Bool
+isLastCase env = and $ map snd env
+----------------------------------------------------------------------
+
+
+isTautology :: Proposition -> Bool
+isTautology p = check p 0
+    where check p n 
+            | isLastCase env = fromJust (evalP env p)
+            | otherwise      = case evalP env p of 
+                                   (Just True ) -> check p (n+1)
+                                   (Just False) -> False
+                                   (Nothing )   -> error "Error 404 var not found"
+            where env = generateCase (vars p) n
+
+-- b = (replicate 10000 ("b",True)) ++ [("a",False)] ++ (replicate 10000 ("b",True))
+-- unAnd = And (Or (Var "a") (Var "b")) (Imp (Cons True) (Neg (And (Var "b") (Var "i"))))
+
+-- Pruebas, borrar despues
+{- 
+main = do let t =[ 
+                    isTautology (Imp (Var "p") (Or (Var "p") (Var "q"))), 
+                    isTautology (Imp (Cons False) (Var "loquesea")), 
+                    isTautology (Or  (Var "p") (Neg (Var "p"))),
+                    isTautology (Imp (Imp (Var "p") (Var "q"))      -- Monotonicity, hue 
+                                     (Imp (Or (Var "p") (Var "r"))
+                                          (Or (Var "q") (Var "r"))
+                                     )
+                                ) 
+                  ]
+          print(and t)
+-}
